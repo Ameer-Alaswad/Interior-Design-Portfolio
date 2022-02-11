35ccd3d06b1c6782882145b0f3b840015c57c561
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-02-11 09:45:17 +0100

    working on navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Navbar from './components/Navbar/header'
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
+      <Navbar />
     </div>
+    </BrowserRouter>
   );
 }
 
