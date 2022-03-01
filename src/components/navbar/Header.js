dd4ed60394fea91a2747775579c2609e18e4bfbd
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-03-01 01:19:07 +0100

    added assets file plus some fine tuning

--- src/components/navbar/Header.js
+++ src/components/navbar/Header.js
@@ -4,13 +4,19 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import BasicMenu from "./dropDownMenu";
+import BasicMenu from "./DropDownMenu";
 import { makeStyles } from "@material-ui/core/styles";
-import { ClassNames } from "@emotion/react";
-
+import { Logo } from "../../utils/assets";
+import { createTheme } from "@mui/material/styles";
 const useStyles = makeStyles({
   navbarButtonsMargin: {
     marginLeft: "25px",
+  },
+});
+const theme = createTheme({
+  typography: {
+    variant: "h6",
+    component: "div",
   },
 });
 export default function Navbar() {
@@ -25,14 +31,14 @@ export default function Navbar() {
         position="static"
       >
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography sx={{ flexGrow: 1 }}>
             <img
               style={{
                 width: "100px",
                 height: "90px",
                 marginTop: "7px",
               }}
-              src="https://cdn.discordapp.com/attachments/301094964086177792/941783904879276073/LOGO.png"
+              src={Logo}
               alt=""
             />
           </Typography>
@@ -42,13 +48,13 @@ export default function Navbar() {
               <BasicMenu />
             </Button>
             <Button className={classes.navbarButtonsMargin} color="inherit">
-              ABOUT US
+              EXPLORE
+            </Button>
+            <Button className={classes.navbarButtonsMargin} color="inherit">
+              PROJECTS
             </Button>
             <Button className={classes.navbarButtonsMargin} color="inherit">
               CONTRACT
-            </Button>
-            <Button className={classes.navbarButtonsMargin} color="inherit">
-              SERVICES
             </Button>
           </Typography>
         </Toolbar>
---
+++ src/utils/assets.js
@@ -0,0 +1,2 @@
+export const Logo =
+  "https://cdn.discordapp.com/attachments/301094964086177792/941783904879276073/LOGO.png";
