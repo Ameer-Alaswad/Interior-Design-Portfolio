65d8ad81d4da9af7155b5464839f4a91f817297d
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-03-07 01:53:05 +0100

    adds PotosGallery component to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/navbar/Header";
 import { BrowserRouter, Route } from "react-router-dom";
+import PhotosGallery from "./components/projects-gallary/projectsGallary";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <PhotosGallery />
       </div>
     </BrowserRouter>
   );
