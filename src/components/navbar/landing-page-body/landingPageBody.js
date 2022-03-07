0feb5a479040655a9c59c01ad4e529b34b4405db
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-03-07 03:44:16 +0100

    adds some styling

--- src/components/navbar/landing-page-body/landingPageBody.js
+++ src/components/navbar/landing-page-body/landingPageBody.js
@@ -3,18 +3,29 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import { CONCRETE } from "../../../default-colors/colors";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   custom: {
     LandingPageBodyContainer: {
+      marginTop: "96px",
+      backgroundColor: CONCRETE,
+      width: "100%",
+
       sx: {
-        width: "100%",
         display: "flex",
         flexDirection: "column",
         textAlign: "center",
-        width: "100%",
       },
+    },
+    sectionsWrapper: {
+      borderBottom: "solid 0.5px #1f1f1f",
+      borderWidth: "50%",
+      fontSize: "32px",
+      width: "55%",
+      margin: "0 auto",
+      opacity: "0.2",
     },
   },
 });
@@ -22,35 +33,36 @@ const theme = createTheme({
 export default function LandingPageBody() {
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={theme.custom.LandingPageBodyContainer.sx}>
+      <Box
+        style={theme.custom.LandingPageBodyContainer}
+        sx={theme.custom.LandingPageBodyContainer.sx}
+      >
         <Typography
-          style={{ border: "solid 2px black" }}
-          variant="h4"
+          style={{
+            fontSize: "32px",
+          }}
+          variant="h2"
           component="div"
           gutterBottom
         >
-          h1. Heading
-        </Typography>
-        <Typography
-          style={{ border: "solid 2px black" }}
-          variant="h4"
-          gutterBottom
-          component="div"
-        >
-          h2. Heading
+          Destination Eichler creates inviting and functional modern interiors
+          <Typography variant="h6" component="div" gutterBottom>
+            SAN FRANCISCO BAY AREA & BEYOND
+          </Typography>
           <Stack spacing={2} direction="row">
             <Button style={{ margin: "0 auto" }} variant="contained">
               Contained
             </Button>
           </Stack>
+          <div style={theme.custom.sectionsWrapper}></div>
         </Typography>
-        <Typography
-          style={{ border: "solid 2px black" }}
-          variant="h4"
-          gutterBottom
-          component="div"
-        >
+        <Typography variant="h4" gutterBottom component="div">
+          h2. Heading
+          <div style={theme.custom.sectionsWrapper}></div>
+        </Typography>
+        <Typography variant="h4" gutterBottom component="div">
           h3. Heading
+          <div style={theme.custom.sectionsWrapper}></div>
         </Typography>
       </Box>
     </ThemeProvider>
