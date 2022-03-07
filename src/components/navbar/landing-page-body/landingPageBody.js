17375b8d27e3797e86deadf4e51236463dfcc20b
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-03-07 02:20:03 +0100

    adds landingPageBody folder and files and landingPageBody component

---
+++ src/components/navbar/landing-page-body/landingPageBody.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import "./landingPageBody.css";
+const LandingPageBody = () => {
+  return <div className="landing-page-body-containor"></div>;
+};
+
+export default LandingPageBody;
