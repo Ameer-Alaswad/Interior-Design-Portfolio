7c807e2c7ed181c36e9e0b5992afcc480f4645ff
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-03-08 14:47:58 +0100

    adds some styling to footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,20 +5,33 @@ export default function Footer() {
   return (
     <Box
       style={{ width: "100%" }}
-      sx={{ display: "flex", justifyContent: "center" }}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
     >
       <img
         alt="img-in-footer"
         src="/carousel/pic1.jpg"
         style={{
           height: "100px",
-          //   width: "99%",
-          //   padding: "5px 5px",
+          width: "99%",
           objectFit: "cover",
           border: "solid 7px white",
+          borderBottom: "none",
         }}
       />
-      <Box style={{ height: "1000px" }}></Box>
+      <Box
+        style={{
+          height: "500px",
+          backgroundColor: "gray",
+          width: "99%",
+          border: "solid 7px white",
+          borderTop: "none",
+        }}
+      ></Box>
     </Box>
   );
 }
