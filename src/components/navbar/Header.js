23662717496beafc9e71a2ee26a866a3e0ce6819
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-03-16 04:44:19 +0100

    makes header responsive

--- src/components/navbar/Header.js
+++ src/components/navbar/Header.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { React, useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -8,12 +8,22 @@ import BasicMenu from "./DropDownMenu";
 import { logo } from "../../utils/assets";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { ZEUS } from "../../default-colors/colors";
+import { makeStyles } from "@mui/styles";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import Divider from "@material-ui/core/Divider";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+export const useStyles = makeStyles(() => ({
+  navbarButtonsMargin: {
+    marginRight: "10px",
+    // [theme.breakpoints.down("sm")]: {},
+  },
+}));
 
 const theme = createTheme({
   custom: {
-    navbarButtonsMargin: {
-      marginLeft: "25px",
-    },
     header: {
       backgroundColor: ZEUS,
       position: "static",
@@ -23,48 +33,79 @@ const theme = createTheme({
       height: "90px",
       marginTop: "7px",
     },
+    menuIconStyling: {
+      color: "white",
+      fontSize: "2.5rem",
+    },
   },
 });
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+  const classes = useStyles();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <ThemeProvider theme={theme}>
-        <AppBar style={theme.custom.header}>
-          <Toolbar>
-            <Typography sx={{ flexGrow: 1 }}>
+        <AppBar position="sticky" style={theme.custom.header}>
+          <Toolbar disableGutters>
+            <Typography style={{ marginLeft: "15px" }} sx={{ flexGrow: 1 }}>
               <img
                 style={theme.custom.logoStyling}
                 src={logo}
                 alt="logo-on-the-navbar"
               />
             </Typography>
-            <Typography>
+            <Hidden xsDown>
+              <Typography style={{ marginRight: "10px" }}>
+                <Button color="inherit">HOME</Button>
+                <Button className={classes.navbarButtonsMargin} color="inherit">
+                  <BasicMenu />
+                </Button>
+                <Button className={classes.navbarButtonsMargin} color="inherit">
+                  EXPLORE
+                </Button>
+                <Button className={classes.navbarButtonsMargin} color="inherit">
+                  PROJECTS
+                </Button>
+                <Button className={classes.navbarButtonsMargin} color="inherit">
+                  CONTRACT
+                </Button>
+              </Typography>
+            </Hidden>
+            <Hidden smUp>
+              <IconButton>
+                <MenuIcon
+                  style={theme.custom.menuIconStyling}
+                  onClick={() => setOpen(true)}
+                />
+              </IconButton>
+            </Hidden>
+            <SwipeableDrawer
+              anchor="right"
+              open={open}
+              onOpen={() => setOpen(true)}
+              onClose={() => setOpen(false)}
+            >
+              <div>
+                <IconButton>
+                  <ChevronRightIcon onClick={() => setOpen(false)} />
+                </IconButton>
+              </div>
+              <Divider />
+
               <Button color="inherit">HOME</Button>
-              <Button
-                className={theme.custom.navbarButtonsMargin}
-                color="inherit"
-              >
+              <Button className={classes.navbarButtonsMargin} color="inherit">
                 <BasicMenu />
               </Button>
-              <Button
-                className={theme.custom.navbarButtonsMargin}
-                color="inherit"
-              >
+              <Button className={classes.navbarButtonsMargin} color="inherit">
                 EXPLORE
               </Button>
-              <Button
-                className={theme.custom.navbarButtonsMargin}
-                color="inherit"
-              >
+              <Button className={classes.navbarButtonsMargin} color="inherit">
                 PROJECTS
               </Button>
-              <Button
-                className={theme.custom.navbarButtonsMargin}
-                color="inherit"
-              >
+              <Button className={classes.navbarButtonsMargin} color="inherit">
                 CONTRACT
               </Button>
-            </Typography>
+            </SwipeableDrawer>
           </Toolbar>
         </AppBar>
       </ThemeProvider>
