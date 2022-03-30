82b2463d1ffffa0e0b581c51b8c3a97b8319c480
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-03-30 19:29:52 +0200

    adds ExploreProjects component and route

---
+++ src/components/explore-projects/ExplorePojects.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export default function ExploreProjects() {
+  return (
+    <div>
+      <p>ameer</p>
+      <p>ameer</p>
+      <p>ameer</p>
+      <p>ameer</p>
+      <p>ameer</p>
+    </div>
+  );
+}
